19d88f2b5e24e8b7119d0c841201020818b9e0e8
andRRuha <lalalamistikc31@gmail.com>
2024-10-22 10:14:39 +0500

    Добавьте файлы проекта.

---
+++ Classes/DbConnection.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterPol.Classes
+{
+    public class DbConnection
+    {
+        public static string ConnectionString = "server=127.0.0.1;port=3307;database=MasterPol;user=root;pwd=;";
+        public static MySqlConnection Connection { get; set; }
+        public MySqlConnection OpenConnection()
+        {
+            Connection = new MySqlConnection(ConnectionString);
+            Connection.Open();
+            return Connection;
+        }
+    }
+}
